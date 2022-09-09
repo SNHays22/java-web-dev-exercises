1f34da02659ce2be33557ee9c2da633b9089731c
SNHays22 <codergrl22@outlook.com>
2022-09-08 20:36:46 -0500

    studio 4 commit

--- src/RestaurantMenu/Menu.java
+++ src/RestaurantMenu/Menu.java
@@ -15,6 +15,46 @@ public class Menu {
     }
 
 
+    //getters & setters
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+
+    @Override
+    public String toString(){
+        String myString = "";
+        for(MenuItem item : this.items){
+            myString += item.getItem() + ": " + item.getPrice() + "\n";
+        }
+        return myString;
+    }
+    //methods
+    public ArrayList<MenuItem> addItem(MenuItem item){
+        this.items.add(item);
+        return this.items;
+    }
+
+    public ArrayList<MenuItem> removeItem(MenuItem item){
+        int toBeRemoved = -1;
+
+        for(int i=0; i<items.size(); i++){
+            if(items.get(i).equals(item)){
+                toBeRemoved = i;
+            }
+        }
+        this.items.remove(toBeRemoved);
+        return this.items;
+    }
 
 }
 //Calendar cal = Calendar.getInstance();
--- src/RestaurantMenu/MenuItem.java
+++ src/RestaurantMenu/MenuItem.java
@@ -17,4 +17,63 @@ public class MenuItem {
         this.isNewItem = isNewItem;
         this.dateItemAdded = dateItemAdded;
     }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isNewItem() {
+        return isNewItem;
+    }
+
+    public void setNewItem(boolean newItem) {
+        isNewItem = newItem;
+    }
+
+    public int getDateItemAdded() {
+        return dateItemAdded;
+    }
+
+    public void setDateItemAdded(int dateItemAdded) {
+        this.dateItemAdded = dateItemAdded;
+    }
+
+    //public static void addItem(String item, String description, String category, double price, boolean isNewItem, int dateItemAdded) {
+        //if
+    //}
+
+   // public addItem(String)
+
+    //verify if item is new - print message about updating menu
+    //compare when menu was updated to if item is new -
+    //  if within say 30 days don't print full menu maybe only print new item menus
 }
+
